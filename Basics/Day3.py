0b4dc01d9bc855a28e922d8a529f8af08fcfebc1
Tanima02 <tanimasinhaece@gmail.com>
2019-07-27 19:12:37 +0000

    commit

---
+++ Basics/Day3.py
@@ -0,0 +1,13 @@
+#import math
+#print(math.sqrt(4))
+#print(math.ceil(8.5))
+#print(math.floor(8.5))
+
+import random
+a=[1,2,3,4]
+print(random.choice(a))
+random.seed(5)
+print(random.choice(a))
+print(random.shuffle(a))
+
+
